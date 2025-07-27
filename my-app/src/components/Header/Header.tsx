d2241fd4f4a478c0da7736b4af3ee55c2b982940
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-07-26 22:05:16 -0600

    Mobile: burger menun closes upon succesful linking

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -9,11 +9,15 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__logo">
-          <Link to="/" className="header__brand">
+          <Link to="/" className="header__brand" onClick={closeMenu}>
             OffCache
           </Link>
         </div>
@@ -21,22 +25,34 @@ const Header = () => {
         <nav className={`header__nav ${isMenuOpen ? "header__nav--open" : ""}`}>
           <ul className="header__nav-list">
             <li className="header__nav-item">
-              <Link to="/" className="header__nav-link">
+              <Link to="/" className="header__nav-link" onClick={closeMenu}>
                 Home
               </Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/community" className="header__nav-link">
+              <Link
+                to="/community"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
                 Community
               </Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/pricing" className="header__nav-link">
+              <Link
+                to="/pricing"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
                 Pricing
               </Link>
             </li>
             <li className="header__nav-item">
-              <Link to="/about" className="header__nav-link">
+              <Link
+                to="/about"
+                className="header__nav-link"
+                onClick={closeMenu}
+              >
                 About
               </Link>
             </li>
@@ -44,10 +60,18 @@ const Header = () => {
         </nav>
 
         <div className="header__actions">
-          <Link to="/signin" className="header__btn header__btn--secondary">
+          <Link
+            to="/signin"
+            className="header__btn header__btn--secondary"
+            onClick={closeMenu}
+          >
             Sign In
           </Link>
-          <Link to="/signup" className="header__btn header__btn--primary">
+          <Link
+            to="/signup"
+            className="header__btn header__btn--primary"
+            onClick={closeMenu}
+          >
             Join Now
           </Link>
         </div>
