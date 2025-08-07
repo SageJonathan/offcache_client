0d18744b518c803fc965fc86cc34f7ab38a8a3eb
Jonathan Sage <sagejonathan.tesol@gmail.com>
2025-08-06 18:26:59 -0600

    fix scss/next bug

--- offcache-next/next.config.js
+++ offcache-next/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   sassOptions: {
     includePaths: ["./src/styles"],
+    prependData: `@import "variables"; @import "mixins";`,
   },
   images: {
     domains: ["images.unsplash.com"],
